72619272cb8895d92cc48cca678bfeb7a3da3209
Romain Deflo <romain.deflo@gmail.com>
2023-05-29 17:37:36 +0200

    add dotenv-rails

--- app/controllers/my/accomodations_controller.rb
+++ app/controllers/my/accomodations_controller.rb
@@ -1,7 +1,29 @@
 module My
   class AccomodationsController < ApplicationController
     def index
-      @accomodations = Accomodation.all
+      @accomodations = Accomodation.where(user: current_user)
+    end
+
+    def new
+      @accomodation = Accomodation.new
+      @user = current_user
+    end
+
+    def create
+      raise
+      @accomodation = Accomodation.new(params_accomodations)
+      @accomodation.user = current_user
+      if @accomodation.save
+        redirect_to accomodation_path(@accomodation)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def params_accomodations
+      params.require(:accomodation).permit(:title, :description, :address, :nb_of_guests, photos: [])
     end
   end
 end
