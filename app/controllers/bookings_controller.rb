e283ae1ef4cd7352d768e74d9dfd2afa0320e927
Amar Slaoua <amar.slaoua@gmail.com>
2023-05-30 14:31:44 +0200

    added the strong param for total price on booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,6 @@ class BookingsController < ApplicationController
   private
 
   def params_booking
-    params.require(:booking).permit(:check_in_date, :check_out_date, :user_id, :accommodation_id, :nb_of_guests)
+    params.require(:booking).permit(:check_in_date, :check_out_date, :user_id, :accommodation_id, :nb_of_guests, :total_price)
   end
 end
