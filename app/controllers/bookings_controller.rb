39c95bc11beca3d170f5f7f731f96c597e83762a
Amar Slaoua <amar.slaoua@gmail.com>
2023-05-30 12:54:06 +0200

    booking form done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @accommodation = Accommodation.find(params[:accommodation_id])
+  end
 
+  def create
+    @booking = Booking.new(params_booking)
+    @booking.user = current_user
+    @accommodation = Accommodation.find(params[:booking][:accommodation_id])
+    if @booking.save
+      redirect_to accommodations_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:check_in_date, :check_out_date, :user_id, :accommodation_id)
+  end
 end
