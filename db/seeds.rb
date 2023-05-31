8ea3d35c88039a057653e081b98b7b5f986cafa7
Romain Deflo <romain.deflo@gmail.com>
2023-05-31 16:16:14 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 require 'faker'
 require 'open-uri'
 
-puts 'creating 10 fake users ...'
-puts 'creating 10 fake accommodations ...'
+Booking.delete_all
+Accommodation.delete_all
+User.delete_all
 
 picture1 = "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg"
 picture2 = "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg"
@@ -14,20 +15,24 @@ picture7 = "https://www.vrbo.com/vacation-ideas/wp-content/uploads/0Bu5ExcClizee
 picture8 = "https://cdn.apartmenttherapy.info/image/upload/v1554209861/at/house%20tours%20stock%20archive/312b3933109935c5f33f6b8ec4a271669eb2d78a.jpg"
 picture9 = "https://wp-tid.zillowstatic.com/bedrock/app/uploads/sites/43/ZG_BrandGTM_0321_GettyImages-528689860-RT-ed6165.jpg"
 picture10 = "https://tatinyhouse.com/wp-content/uploads/2022/10/13-Sunshine-03.jpg"
-picture11 = "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+picture11 = "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg"
+picture12 = "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
-pictures = [picture1, picture2, picture3, picture4, picture5, picture6, picture7, picture8, picture9, picture10]
+pictures = [picture1, picture2, picture3, picture4, picture5, picture6, picture7, picture8, picture9, picture10, picture11]
 counter = 0
 
-2.times do
+puts 'creating 10 fake users ...'
+puts 'creating 10 fake accommodations ...'
+
+11.times do
   new_user = User.new(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: '123456',
-    bio: "Hello there! I’m your friendly and enthusiastic host for your upcoming stay in this beautiful city. As a born and raised local, I know all the hidden gems and insider tips to make your visit truly memorable. I have a passion for hosting and creating unique experiences for my guests. I take great pride in providing a comfortable and welcoming atmosphere, ensuring that you feel right at home during your stay. Whether you’re here for a short getaway or an extended vacation, I’ll go above and beyond to make sure your time here exceeds your expectations."
+    bio: "Hello there! I'm your friendly and enthusiastic host for your upcoming stay in this beautiful city. As a born and raised local, I know all the hidden gems and insider tips to make your visit truly memorable. I have a passion for hosting and creating unique experiences for my guests. I take great pride in providing a comfortable and welcoming atmosphere, ensuring that you feel right at home during your stay. Whether you're here for a short getaway or an extended vacation, I'll go above and beyond to make sure your time here exceeds your expectations."
   )
-  file = URI.open(picture11)
+  file = URI.open(picture12)
   new_user.photo.attach(io: file, filename: "avatar.png", content_type: "image/jpg")
   new_user.save!
   new_acco = Accommodation.new(
@@ -46,20 +51,22 @@ end
 
 puts '10 fake users created !'
 puts '10 fake accommodations created ! '
+
 puts 'creating 1 super fake accommodation (several coherent pictures) ...'
 new_user = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   email: Faker::Internet.email,
   password: '123456',
-  bio: "Hello there! I’m your friendly and enthusiastic host for your upcoming stay in this beautiful city. As a born and raised local, I know all the hidden gems and insider tips to make your visit truly memorable. I have a passion for hosting and creating unique experiences for my guests. I take great pride in providing a comfortable and welcoming atmosphere, ensuring that you feel right at home during your stay. Whether you’re here for a short getaway or an extended vacation, I’ll go above and beyond to make sure your time here exceeds your expectations."
+  bio: "Hello there! I'm your friendly and enthusiastic host for your upcoming stay in this beautiful city. As a born and raised local, I know all the hidden gems and insider tips to make your visit truly memorable. I have a passion for hosting and creating unique experiences for my guests. I take great pride in providing a comfortable and welcoming atmosphere, ensuring that you feel right at home during your stay. Whether you're here for a short getaway or an extended vacation, I'll go above and beyond to make sure your time here exceeds your expectations."
 )
+file = URI.open(picture12)
+new_user.photo.attach(io: file, filename: "avatar.png", content_type: "image/jpg")
 new_user.save!
 super_picture1 = "https://i.pinimg.com/originals/72/5e/d6/725ed67a503311cfa08ea07fc3835981.jpg"
 super_picture2 = "https://media.architecturaldigest.com/photos/5d2f3540dea3bc0008636368/16:9/w_2580,c_limit/After-Photo-7.jpg"
 super_picture3 = "https://cdn.trendir.com/wp-content/uploads/old/archives/2015/12/07/1-kitchen-design-lofts-3-urban-ideas-snaidero.jpg"
 super_picture4 = "https://homedesignlover.com/wp-content/uploads/2016/04/4-loft-loft.jpg"
-
 super_pictures = [super_picture1, super_picture2, super_picture3, super_picture4]
 new_acco = Accommodation.new(
   title: Faker::Space.star,
