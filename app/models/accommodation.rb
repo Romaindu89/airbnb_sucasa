59135914d7c716a0bb1a9b9c3abacbcb2d571824
Amar Slaoua <amar.slaoua@gmail.com>
2023-06-01 10:44:05 +0200

    added validations for title, address, price_per_night and nb_of_guests

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -2,4 +2,14 @@ class Accommodation < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :bookings
+
+  validates :title, presence: true
+
+  validates :address, presence: true
+
+  validates :price_per_night, presence: true
+  validates :price_per_night, length: { minimum: 1 }
+
+  validates :nb_of_guests, presence: true
+  validates :nb_of_guests, length: { minimum: 1 }
 end
