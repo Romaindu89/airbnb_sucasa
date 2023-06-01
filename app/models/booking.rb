c76ef8270531e2e2f16101cbd43f93f5b3761790
Amar Slaoua <amar.slaoua@gmail.com>
2023-06-01 11:06:27 +0200

    added validations for booking model : check_out_date must be greater than check_in and nb_of_guests can't be blank

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :accommodation
+
+  validates :nb_of_guests, presence: true
+  validates :check_in_date, presence: true
+  validates :check_out_date, presence: true, comparison: { greater_than: :check_in_date}
 end
